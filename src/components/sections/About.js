d23fdc2c4c55dee63059cc815126616e374f74d4
Ozan Gokberk <ozan8@hotmail.co.uk>
2022-11-10 13:09:19 +0000

    created typewriting effect

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -7,13 +7,12 @@ function About() {
         <p className={styles.title}>About</p>
         <div className={styles.card}>
           <p>
-            I am currently in my 2nd year of Computer Science. I am ambitious
-            and hardworking. Learning is a passion of mine. I am always looking
-            for new opportunities to learn and grow. I enjoy reading fictional
-            and non fiction books. I also enjoy playing Chess. I started
-            learning how to code in 2019 and have been learning ever since. I am
+            Currently I'm in my 2nd year of Computer Science. I am ambitious
+            and hardworking, learning is a passion of mine. I am always looking
+            for new opportunities to learn and grow. In my free time i enjoy reading fictional
+            and non fiction books. Also, I enjoy playing Chess and have been playing for one year. 
+            I started learning how to code in 2019 and have been learning ever since. I am
             currently learning backend development using Node.js and Express.js.
-            I am also learning how to use MongoDB.
           </p>
           <p className={styles.languages}>Known Languages</p>
           <ul className={styles.skills}>
